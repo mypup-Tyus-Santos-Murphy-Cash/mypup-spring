322a760b26d95e685188e4d475ea94900fe7c688
Paris Tyus <Tyus.Paris1@gmail.com>
2020-03-27 09:47:35 -0500

    inital default file setup

---
+++ src/main/java/com/mypup/demo/MypupApplication.java
@@ -0,0 +1,13 @@
+package com.mypup.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MypupApplication.class, args);
+    }
+
+}
