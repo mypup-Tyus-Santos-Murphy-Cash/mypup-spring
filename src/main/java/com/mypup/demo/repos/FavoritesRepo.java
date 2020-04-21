84177e5d32ba73b379b1e5fc421df74ff0957251
Matthew <murphy.d.matthew28@gmail.com>
2020-04-21 10:35:40 -0500

    'updated'

--- src/main/java/com/mypup/demo/repos/FavoritesRepo.java
+++ src/main/java/com/mypup/demo/repos/FavoritesRepo.java
@@ -3,11 +3,9 @@ import com.mypup.demo.models.Favorites;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 import java.util.Optional;
 
 public interface FavoritesRepo extends JpaRepository<Favorites, Long> {
     @Query("Select favorites  FROM Favorites favorites WHERE favorites.user.id= :user_id and favorites.dogPost=:dog_post_id")
-    Optional<Favorites> getCartByProductIdAnduserId(@Param("user_id")Long user_id, @Param("dog_post_id")Long dog_post_id);
+    Optional<Favorites> getFavoritesByUserAndDogPost(@Param("user_id")Long user_id, @Param("dog_post_id")Long dog_post_id);
 }
