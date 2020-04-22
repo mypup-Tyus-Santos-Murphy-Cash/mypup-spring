eed1ea0bb74b00cc72987072f35432a6d63237c5
Paris Tyus <Tyus.Paris1@gmail.com>
2020-04-22 17:14:49 -0500

    still woprking on favorites

--- src/main/java/com/mypup/demo/controllers/FavoritesController.java
+++ src/main/java/com/mypup/demo/controllers/FavoritesController.java
@@ -1,27 +1,43 @@
 package com.mypup.demo.controllers;
 
 
+import com.mypup.demo.models.DogPost;
 import com.mypup.demo.models.Favorites;
+import com.mypup.demo.repos.DogPostRepo;
+import com.mypup.demo.repos.FavoritesRepo;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
 
 @Controller
 public class FavoritesController {
+    private DogPostRepo dogPostDao;
+    private FavoritesRepo favoritesDao;
 
+    public FavoritesController(DogPostRepo dogPostDao, FavoritesRepo favoritesDao) {
+        this.dogPostDao = dogPostDao;
+        this.favoritesDao = favoritesDao;
+    }
 
-    @PosttMapping("/favorites")
-    public List<Favorites> favoritesList(@RequestBody HashMap<String,String> addFavoriteRequest){
-
-    return addFavoriteRequest;
+    @PostMapping("/buyer-profile/{id}/addToFavorites")
+    public String addToFavorites(@PathVariable long id, @RequestParam String dogBreed, @RequestParam String dogGroup, @RequestParam String dogDescription, @RequestParam String dogPrice, @RequestParam String images) {
+        Favorites addToFavorites = favoritesDao.getOne(id);
+        addToFavorites.setDogBreed(dogBreed);
+        addToFavorites.setDogGroup(dogGroup);
+        addToFavorites.setDogDescription(dogDescription);
+        addToFavorites.setDogPrice(dogPrice);
+        addToFavorites.setImages(images);
+        favoritesDao.save(addToFavorites);
+        return "redirect:/buyer-profile";
     }
 
     @GetMapping()
-    remove from favorites
+    public String removeFromFavorites(){
+
+        return "redirect:/buyer-profile";
+    }
 
 }
